fbcbb94e2edf6678fda6f5f8f9e4000bc2ea52e7
akshaya <acchutalli2k6@gmail.com>
2026-06-09 11:43:20 +0530

    Create freq_div.v

---
+++ freq_div.v
@@ -0,0 +1,13 @@
+module freq_div #(
+    parameter TARGET = 27'h2FAF07F // Default value for real hardware
+)(
+input clk, rst, output reg pclk
+    );
+// period =10ns
+reg [26:0] count;
+    always @(posedge clk) begin
+    if(!rst) begin count<=0; pclk<=1'b1; end
+    else if(count==TARGET) begin count<=0; pclk<=~pclk; end
+    else count<=count+1; 
+    end
+endmodule
